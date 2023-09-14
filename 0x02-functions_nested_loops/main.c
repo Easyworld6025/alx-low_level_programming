9535ea5c9d70d5426f950660a9d95aa3c571c790
easyworld6025 <owolabi1696@gmail.com>
2023-09-14 05:32:33 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+n.c                9-times.c
+4-isalpha               9-times_table.c
+4-isalpha.c             main.h
+4-main.c                _putchar.c
+5-main.c                README.md
+5-sign
+root@2b160a53b1d5:~/alx-low_level_programming/0x02-functions_nested_loops# vi 9-main.c
+root@2b160a53b1d5:~/alx-low_level_programming/0x02-functions_nested_loops#
+
