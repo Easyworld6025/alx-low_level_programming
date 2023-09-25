bd5cd150d2944e3da47ddaa2dccd975e20aea28d
easyworld6025 <owolabi1696@gmail.com>
2023-09-25 15:40:23 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@
  * @needle: input
  * Return: Always 0 (success)
  */
-char *_strstr(char *heystack, char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
