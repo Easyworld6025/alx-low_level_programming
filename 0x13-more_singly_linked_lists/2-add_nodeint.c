3db63a4305e1f75afbf992a68103c0126b683c0b
easyworld6025 <owolabi1696@gmail.com>
2023-10-26 10:36:35 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t);
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	if (*head == NULL)
