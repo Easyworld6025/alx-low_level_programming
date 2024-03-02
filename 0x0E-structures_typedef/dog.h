5a6672030b5f7fb831c7a4f2348f3e1d5215aa3e
Easyworld6025 <owolabi1696@gmail.com>
2024-03-02 15:45:05 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,18 @@
  * Description: first struct with alx
  */
 
-typedef struct dog
+/**
+ * dog_t - new name for struct dog
+ */
+
+typedef struct dog dog_t;
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
