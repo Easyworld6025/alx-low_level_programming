d5a7bf6802224dd5a5976ffbebbc37263c3394ad
Easyworld6025 <owolabi1696@gmail.com>
2024-03-03 11:39:37 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 /**
  * new_dog - new dog to be created
  * @name: name if the new dog
@@ -9,7 +9,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int n, o, i;
+	unsigned int nl, ol, i;
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
@@ -17,10 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog ==  NULL)
 		return (NULL);
-	for (n = 0; name[n]; n++)
+	for (nl = 0; name[nl]; nl++)
 		;
-	n++;
-	dog->name = malloc(n * sizeof(char));
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
@@ -29,17 +29,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < n; i++)
 		dog->name[i] = name[i];
 	dog->age = age;
-	for (o = 0; owner[o]; o++)
+	for (ol = 0; owner[ol]; ol++)
 		;
-	o++;
-	dog->owner = malloc(o * sizeof(char));
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->owner);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < o; i++)
+	for (i = 0; i < ol; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
 }
