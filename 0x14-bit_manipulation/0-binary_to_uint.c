8aa666508fa24d1e7ff6fd4edd0170e7a2117a74
Easyworld6025 <owolabi1696@gmail.com>
2024-03-04 13:10:38 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,8 +20,10 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-			return (b[0] - 48);
-	for (i = 0; b[i] != '\0'; i++)
+	{
+		return (b[0] - 48);
+	}
+		for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		return (0);
